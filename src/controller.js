51473b549b5b2a765491de86a7f7487115fd2d0e
ClaudiaGarfias <me@claudiagarfias.works>
2019-03-30 13:18:50 -0600

    Trabajando en crear posts y almacenar en firestore desde timeline screen con boton publicar. Todavía no funcional. Se hace commit para cambiar a rama "pruebas-funcion-pura-alDOM"

--- src/controller.js
+++ src/controller.js
@@ -10,49 +10,78 @@ function handleSignedOutUser() {
 
 function handleSignedInUser(firebaseUser) {
     location.hash = "#timeline";
-    contentDiv.innerHTML = 
-    `<section class = "timeline-screen">
-        <nav id = "timeline-nav" class= "timeline-nav flex-row">
-            <h1>madame{code}</h1>
-            <a href="">Salir<img src=""></a>
-        </nav>
+    
+    const bringTimelineContent = () => {
+        contentDiv.innerHTML = 
+        `<section class = "timeline-screen">
+            <nav id = "timeline-nav" class= "timeline-nav flex-row">
+                <h1>madame{code}</h1>
+                <a href="">Salir<img src=""></a>
+            </nav>
 
-        <div id="user-info"></div>
+            <div id="user-info"></div>
 
-        <article id="create-post" class="create-post">
-            <h2>Crear Publicación:</h2>
-            <input id="input-post" type="text" placeholder="¿Qué quieres compartir con la comunidad?">
-            <p>Visible para:</p>
-            <input type="radio" name="public" value="public"> Público
-            <input type="radio" name="friends" value="friends"> Sólo Amigos
-            <br>
-            <button>Publicar</button>
-        </article>
-    </section>
-
-    <section id="all-posts">
-        <h2>Publicaciones recientes:</h2>
-        <article id="display-posts">
-            <div id ="own-post">
-                <p id="user-email"><span class="strong">micorreo@es.com</span> compartió:</p><button id="edit-post">Edit</button>
+            <article id="create-post" class="create-post">
+                <h2>Crear Publicación:</h2>
+                <input id="input-post" type="text" placeholder="¿Qué quieres compartir con la comunidad?">
+                <p>Visible para:</p>
+                <input type="radio" name="post-visibility" value="public"> Público
+                <input type="radio" name="post-visibility" value="friends"> Sólo Amigos
                 <br>
-                <p id="publication" contenteditable="false">Texto EDITABLE de la publicación:</p>
-                <br>
-                <button id="like-post">like</button>
-            </div>
-            <div id ="other-post">
-                    <p id="user-email"><span class="strong">correodeotros@es.com</span> compartió:</p>
+                <button id="publish-post">Publicar</button>
+            </article>
+        </section>
+
+        <section id="all-posts">
+            <h2>Publicaciones recientes:</h2>
+            <article id="display-posts">
+                <div id ="own-post">
+                    <p id="user-email"><span class="strong">micorreo@es.com</span> compartió:</p><button id="edit-post">Edit</button>
                     <br>
-                    <p id="publication">Texto de la publicación:</p>
+                    <p id="publication" contenteditable="false">Texto EDITABLE de la publicación:</p>
                     <br>
                     <button id="like-post">like</button>
-            </div>
-        </article>
-    </section>
+                </div>
+                <div id ="other-post">
+                        <p id="user-email"><span class="strong">correodeotros@es.com</span> compartió:</p>
+                        <br>
+                        <p id="publication">Texto de la publicación:</p>
+                        <br>
+                        <button id="like-post">like</button>
+                </div>
+            </article>
+        </section>
     `;
+}
     const userInfo = document.getElementById('user-info');
     userInfo.innerHTML =    `ID de usuario: ${firebaseUser.uid}<br>
                             Email: ${firebaseUser.email}<br>
                             Nombre: ${firebaseUser.displayName}`;
+    
+    const publishBtn = document.getElementById('publish-post');
+    const visibility = document.querySelectorAll('post-visibility');
+    console.log(visibility); //WHAT is visibility?
+    const visibilityArr = Array.prototype.slice.call(visibility);
+    console.log(visibilityArr); //WHAT is visibilityArr?
+
+    function getVisibilityVal() {
+        let val;
+        // get list of radio buttons with specified name
+        var radios = visibilityArr;
+        
+        // loop through list of radio buttons
+        for (var i=0, len=visibilityArr.length; i<len; i++) {
+            if ( visibilityArr[i].checked ) { // radio checked?
+                val = visibilityArr[i].value; // if so, hold its value in val
+                break; // and break out of for loop
+            }
+        }
+        console.log(val);
+        return val; // return value of checked radio or undefined if none checked
+    }
+
+    const addPost = () => {
+
+    }
 }
 
--- src/firestore-manage.js
+++ src/firestore-manage.js
@@ -3,25 +3,37 @@
 // // Required for side-effects
 // require("firebase/firestore");
 
-console.log(`Hi Firestore is here!`);
+console.log(`HELLO! Firestore is here!`);
 
 // Initialize Cloud Firestore through Firebase
 let db = firebase.firestore();
 
+//////Add DATA to FIRESTORE ///
 
 
+//LEARNING COLLECTION
+  db.collection("theClaudiaLearningCollection").add({ //creates COLLECTION "users" then adds a DOCUMENT into it
+    first: "Claudia",
+    last: "Garfias",
+    born: 1914,
+    profilePic: "https://claudiagarfias.works/contents/uploads/media/claudia-garfias-web-developer-profile-photo.jpg?"
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id); //DOCUMENT ID
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error.code);//Catches ERROR
+});
 
-
-
-//Add Data to FIRESTORE 
-//   db.collection("users").add({ //creates COLLECTION "users" then adds DOCUMENT
-//     first: "Claudia",
-//     last: "Garfias",
-//     born: 1914,
-//     profilePic: "https://claudiagarfias.works/contents/uploads/media/claudia-garfias-web-developer-profile-photo.jpg?"
+// //POSTS COLLECTION
+// db.collection("ourSocialAppPosts").add({ //creates COLLECTION "users" then adds a DOCUMENT into it
+//     postText: "Lorem Ipsum Texto de la publicación",
+//     //postImage: "https://claudiagarfias.works/contents/uploads/media/claudia-garfias-web-developer-profile-photo.jpg?",
+//     likes: [],
+//     postvisibility: "public"
 // })
 // .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id); //DOCUMENT ID
+//     console.log("ID del POST: ", docRef.id); //DOCUMENT ID
 // })
 // .catch(function(error) {
 //     console.error("Error adding document: ", error.code);//Catches ERROR
